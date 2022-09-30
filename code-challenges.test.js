75955a0afb98a7a38ab98b5e549ffbf7a2bba8c9
let <kyle.tran.le@gmail.com>
2022-09-30 16:25:00 -0700

    figured out #2 even though it took an hour, but all done

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -7,8 +7,7 @@
 
 
 // PSEUDOCODE:
-
-    // create a test starting with describe and the function name "fib"
+// create a test starting with describe and the function name "fib" with the information provided and desired output
 
     describe("fib", () => {
           const fibLength1 = 6
@@ -21,13 +20,12 @@
         })
     })
 
-    // b) Create the function that makes the test pass.
+// b) Create the function that makes the test pass.
 
-
-    //Pseudo code declare a function named fib
-    // create a for loop that iterates through the number length
-    // start for loop at i = 2 since there are already 2 items in array
-    // make a new index in array that equals to the sum of the previous 2 numbers
+ //Pseudo code declare a function named fib
+// create a for loop that iterates through the number length
+// start for loop at i = 2 since there are already 2 items in array
+// make a new index in array that equals to the sum of the previous 2 numbers
 
         const fib = (num) => {
             let array = [1, 1]
@@ -42,57 +40,52 @@
 // --------------------2) Create a function that takes in an object and returns an array of the numbers sorted from least to greatest.
 // Hint: Check out this resource: Object.values() https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_objects/Object/values
 
-
 // a) Create a test with expect statements for each of the variables provided.
 
+// PSEUDOCODE: create a test starting with describe and the function name "leastToGreatest" with the information provided and the desired output
 
-const studyMinutesWeek1 = {
-  sunday: 90,
-  monday: 30,
-  tuesday: 20,
-  wednesday: 15,
-  thursday: 30,
-  friday: 15,
-  saturday: 60
-}
-// Expected output: [15, 15, 20, 30, 30, 60, 90]
-
-const studyMinutesWeek2 = {
-  sunday: 100,
-  monday: 10,
-  tuesday: 45,
-  wednesday: 60,
-  thursday: 20,
-  friday: 15,
-  saturday: 65
-}
-// Expected output: [10, 15, 20, 45, 60, 65, 100]
-
-
-    describe("leastToGreatest", () => {
-        it("takes in an object and returns an array of the numbers sorted from least to greatest", () => {
-            expect(leastToGreatest(studyMinutesWeek1)).toEqual([15, 15, 20, 30, 30, 60, 90])
-            expect(leastToGreatest(studyMinutesWeek2)).toEqual([10, 15, 20, 45, 60, 65, 100])
-        })
+describe("leastToGreatest", () => {
+    it(`Takes in an object and returns an array of the numbers sorted from least to greatest.`, () => {
+      const studyMinutesWeek1 = {
+        sunday: 90,
+        monday: 30,
+        tuesday: 20,
+        wednesday: 15,
+        thursday: 30,
+        friday: 15,
+        saturday: 60,
+      }
+      const studyMinutesWeek2 = {
+        sunday: 100,
+        monday: 10,
+        tuesday: 45,
+        wednesday: 60,
+        thursday: 20,
+        friday: 15,
+        saturday: 65,
+      }
+      expect(leastToGreatest(studyMinutesWeek1)).toEqual([15, 15, 20, 30, 30, 60, 90])
+      expect(leastToGreatest(studyMinutesWeek2)).toEqual([10, 15, 20, 45, 60, 65, 100])
     })
-
-
+  })
+  
+  
 // b) Create the function that makes the test pass.
+// PSEUDOCODE: created a function named leastToGreatest, created an array to pass through wanted array
+//at first I thought of using the method filter but that did not work, and then i found the method .sort 
 
-    const leastToGreatest = (object) => {  
-        let filteredArr = object.filter(value => typeof value === "number")
-        object.sort((a,b) => a-b)
-        return object
-    }
-
-    //output: failed
+  const leastToGreatest = (object) => {
+    emptyArr = Object.values(object).sort((a, b) => a - b)
+    return emptyArr
+  }
+  //output: PASS
 
 // --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
 
 
 // a) Create a test with expect statements for each of the variables provided.
 
-    //Pseudo code describe a function named accSum
+// PSEUDOCODE: create a test starting with describe and the function name "accSum" with the information provided and the desired output
   
     describe("accSum", () => {
       const accountTransactions1 = [100, -17, -23, -9]
@@ -109,8 +102,8 @@ const studyMinutesWeek2 = {
     })
 
 // b) Create the function that makes the test pass.
-
-
+// PSEUDOCODE: naming the function accSum, creating an empty array (sumArr) so the updated sum can be inputed into it
+//made a forloop to iterate through old array and to get the some of each index
     const accSum = (numArr) => {
         let sumArr = []
         if (numArr.length > 0) {
@@ -123,4 +116,3 @@ const studyMinutesWeek2 = {
     }
 
     //output:PASS
-
